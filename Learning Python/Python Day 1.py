8cccb528a8a7ef0556a1c3b57b7a3057186c9625
Heckley1998 <58855366+Heckley1998@users.noreply.github.com>
2021-01-08 22:35:57 +0000

    Delete Python Day 1.py

--- Learning Python/Python Day 1.py
+++
@@ -1,9 +0,0 @@
-character_name = "Connor"
-character_age = 22
-is_Male = True
-print("There once was a man named " + character_name + ",")
-print("He was "  + character_age + " years old.")
-
-character_name = "Phil"
-print("He really liked the name " + character_name + ",")
-print("but didnt like being "+ character_age + ",")
